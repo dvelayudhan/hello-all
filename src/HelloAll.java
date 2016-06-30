5f9caf206567dba66de6ffff978ac3f91330d04b
dvelayudhan <devika.velayundhan@vml.com>
2016-06-30 16:02:45 +0100

    committing changes to branch

--- src/HelloAll.java
+++ src/HelloAll.java
@@ -3,8 +3,12 @@ public class HelloAll {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World !");
-		System.out.println("I am saying another hello");
+		System.out.println("Hello World !"); //With this line of code, I created the project in eclipse and uploaded to github by creating a local repo first and then pushing from local to remote(github)
+		System.out.println("I am saying another hello");  // With this line of code , I made a change to project in eclipse , added this change to local repo , committed and pushed to github
+		//I cloned the project in remote to local 
+		//I created a new branch of the project newfeature
+		//I imported this newfeature branch to eclipse 
+		System.out.println("Hi from me again"); //With this line of code I made a change to the newfeature branch
 	}
 
 }
