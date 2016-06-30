21cb6256d87dce661900e1a1bad3a13e725117ac
dvelayudhan <devika.velayundhan@vml.com>
2016-06-30 14:25:02 +0100

    commit change

--- src/HelloAll.java
+++ src/HelloAll.java
@@ -4,6 +4,7 @@ public class HelloAll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !");
+		System.out.println("I am saying another hello");
 	}
 
 }
